02ac782c3673d501c334ca953ba2db4e34c64013
ovie012 <124385756+ovie012@users.noreply.github.com>
2023-08-20 19:16:14 +0100

    Add files via upload

---
+++ legend-dev.js
@@ -0,0 +1,57 @@
+// FOR THE LEFT SLIDE IN ANIMATIONS
+
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', checkBoxes);
+
+checkBoxes();
+
+function checkBoxes() {
+    const triggerBottom = window.innerHeight / 5 * 4;
+
+    boxes.forEach((box) => {
+        const boxTop = box.getBoundingClientRect().top;
+
+        if (boxTop < triggerBottom) {
+            box.classList.add('show');
+        } else {
+            box.classList.remove('show');
+        }
+    });
+}
+
+
+// FOR THE RIGHT SLIDE IN ANIMATIONS 
+
+const boxesR = document.querySelectorAll('.boxR');
+
+window.addEventListener('scroll', checkBoxesR);
+
+checkBoxesR();
+
+function checkBoxesR() {
+    const triggerBottomR = window.innerHeight / 5 * 4;
+
+    boxesR.forEach((box) => {
+        const boxTop = box.getBoundingClientRect().top;
+
+        if (boxTop < triggerBottomR) {
+            box.classList.add('show');
+        } else {
+            box.classList.remove('show');
+        }
+    });
+}
+
+
+// JAVASCRIPT FOR THE NAV SIDEBAR
+
+const navClick = document.querySelector(".sidebar .toggle-btn");
+
+const navSidebar = document.querySelector(".sidebar");
+
+function navClicked() {
+    navSidebar.classList.toggle("active");
+};
+
+navClick.addEventListener('click', navClicked);
